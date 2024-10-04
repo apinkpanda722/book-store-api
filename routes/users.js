902bb24165073868388d0659bdcfcd9c5d281e30
이진혁 <iih907@mediazen.co.kr>
2024-10-04 16:52:10 +0900

    회원 가입 API 구현

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const conn = require("../mariadb");
 
 router.use(express.json());
 
 router.post("/join", (req, res) => {
+    const { email, password } = req.body;
+
+    let sql = 'INSERT INTO users (email, password) VALUES (?,?)';
+    let values = [email, password];
+
+    conn.query(sql, values, (error, result) => {
+        if (error) {
+            console.log(error);
+            return res.status(400).end(result);
+        }
+
+        res.status(201).json();
+    })
+
     res.json("회원 가입");
 });
 router.post("/login", (req, res) => {
