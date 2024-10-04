b6ab62d8ae5dc1e5d7db9e4f553aab39a2f3855b
이진혁 <iih907@mediazen.co.kr>
2024-10-04 17:04:40 +0900

    http-status-codes 모듈 import

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
+const { StatusCodes } = require('http-status-codes');
 
 router.use(express.json());
 
@@ -13,20 +14,21 @@ router.post("/join", (req, res) => {
     conn.query(sql, values, (error, result) => {
         if (error) {
             console.log(error);
-            return res.status(400).end(result);
+            return res.status(StatusCodes.BAD_REQUEST).end();
         }
 
-        res.status(201).json();
-    })
-
-    res.json("회원 가입");
+        return res.status(StatusCodes.CREATED).json(result);
+    });
 });
+
 router.post("/login", (req, res) => {
     res.json("로그인");
 });
+
 router.post("/reset", (req, res) => {
     res.json("비밀번호 초기화 요청");
 });
+
 router.put("/reset", (req, res) => {
     res.json("비밀번호 초기화");
 });
