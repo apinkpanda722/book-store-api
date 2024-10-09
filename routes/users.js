c1a49f12dc1a83da7978855dc07a0fbaa287f9c2
derulo taser <iih907@naver.com>
2024-10-09 16:42:35 +0900

    [feat] 컨트롤러 생성

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const conn = require("../mariadb");
-const { StatusCodes } = require('http-status-codes');
+const {login, join, passwordResetRequest, passwordReset} = require("../controller/UserController");
 
 router.use(express.json());
 
-router.post("/join", (req, res) => {
-    const { email, password } = req.body;
+router.post("/join", join());
 
-    let sql = 'INSERT INTO users (email, password) VALUES (?,?)';
-    let values = [email, password];
+router.post("/login", login());
 
-    conn.query(sql, values, (error, result) => {
-        if (error) {
-            console.log(error);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+router.post("/reset", passwordResetRequest());
 
-        return res.status(StatusCodes.CREATED).json(result);
-    });
-});
-
-router.post("/login", (req, res) => {
-    res.json("로그인");
-});
-
-router.post("/reset", (req, res) => {
-    res.json("비밀번호 초기화 요청");
-});
-
-router.put("/reset", (req, res) => {
-    res.json("비밀번호 초기화");
-});
+router.put("/reset", passwordReset());
 
 module.exports = router;
