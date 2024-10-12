c8b254a4aced912ba4710da67401873baa470db6
derulo taser <iih907@naver.com>
2024-10-12 20:15:19 +0900

    [feature] 카테고리별, 신간, 도서 목록 조회 작업

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,42 +2,43 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-    let { category_id } = req.query;
+    let { category_id, isNew } = req.query;
 
-    if (category_id) {
-        // 카테고리별 도서 목록 조회
-        let sql = 'SELECT * FROM books WHERE category_id = ?';
-        conn.query(sql, category_id,
-            (error, results) => {
-                if (error) {
-                    console.log(error);
-                    return res.status(StatusCodes.BAD_REQUEST).end();
-                }
+    let sql = 'SELECT * FROM books ';
+    let values = [];
+    if (category_id && isNew) {
+        sql += 'WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+        values = [category_id, isNew];
+    } else if (category_id) {
+        sql += 'WHERE category_id = ?';
+        values = category_id;
+    } else if (isNew) {
+        sql += 'WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
+        values = isNew;
+    }
 
-                if (results.length)
-                    return res.status(StatusCodes.OK).json(results);
-                else
-                    return res.status(StatusCodes.NOT_FOUND).end();
-            })
-    } else {
-        // 전체 도서 목록 조회
-        let sql = 'SELECT * FROM books';
-        conn.query(sql, (error, results) => {
+    // 카테고리별 도서 목록 조회
+    conn.query(sql, values,
+        (error, results) => {
             if (error) {
                 console.log(error);
-                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+                return res.status(StatusCodes.BAD_REQUEST).end();
             }
 
-            return res.status(StatusCodes.OK).json(results);
-        })
-    }
+            if (results.length)
+                return res.status(StatusCodes.OK).json(results);
+            else
+                return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    )
 
 }
 const bookDetail = (req, res) => {
     let { id } = req.params;
 
     id = parseInt(id);
-    let sql = 'SELECT * FROM books WHERE id = ?';
+    let sql = `SELECT * FROM books LEFT JOIN category
+        ON books.category_id = category.id WHERE books.id = ?`;
     conn.query(sql, id,
         (error, results) => {
         if (error) {
