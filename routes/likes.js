d26ce355a06715ef4dcbad96d1f892a329c13795
derulo taser <iih907@naver.com>
2024-10-13 00:11:22 +0900

    [feature] 좋아요 추가, 삭제 작업

--- routes/likes.js
+++ routes/likes.js
@@ -1,16 +1,16 @@
 const express = require('express');
+const {
+    addLike,
+    removeLike
+} = require("../controller/LikeController");
 const router = express.Router();
 
 router.use(express.json());
 
 // 좋아요 추가
-router.post('/:id', (req, res) => {
-    res.json("좋아요 추가");
-});
+router.post('/:id', addLike);
 
 // 좋아요 삭제
-router.delete('/:id', (req, res) => {
-    res.json("좋아요 삭제");
-});
+router.delete('/:id', removeLike);
 
 module.exports = router;
