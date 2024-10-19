e15d306f4cf7e3ffac13611fe0b249b108867ea5
derulo taser <iih907@naver.com>
2024-10-19 22:58:13 +0900

    [feature] 좋아요 추가, 취소 API에 JWT 추가 작업

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -1,12 +1,15 @@
+const jwt = require("jsonwebtoken");
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const addLike = (req, res) => {
-    const { id } = req.params;
-    const { user_id } = req.body;
+    const book_id = req.params.id;
+    let authorization = decodeJwt(req);
 
     let sql = 'INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)';
-    let values = [user_id, id];
+    let values = [authorization.id, book_id];
     conn.query(sql, values,
         (error, results) => {
             if (error) {
@@ -19,11 +22,11 @@ const addLike = (req, res) => {
 }
 
 const removeLike = (req, res) => {
-    const { id } = req.params;
-    const { user_id } = req.body;
+    const book_id = req.params.id;
+    let authorization = decodeJwt(req);
 
     let sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?';
-    let values = [user_id, id];
+    let values = [authorization.id, book_id];
     conn.query(sql, values,
         (error, results) => {
             if (error) {
@@ -33,6 +36,12 @@ const removeLike = (req, res) => {
 
             return res.status(StatusCodes.OK).json(results);
         })
+}
+
+function decodeJwt (req) {
+    let token = req.headers["authorization"];
+    let decodedJwt = jwt.verify(token, process.env.PRIVATE_KEY);
+    return decodedJwt;
 }
 
 module.exports = {
