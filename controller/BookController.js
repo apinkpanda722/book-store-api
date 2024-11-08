9a05f9ed9871e67e65653673c1d246e5dfcb44fd
derulo taser <iih907@naver.com>
2024-11-08 21:08:18 +0900

    [fix] 도서 쿼리 조회시 isNew -> news으로 쿼리 파라미터 수정

--- controller/BookController.js
+++ controller/BookController.js
@@ -5,20 +5,20 @@ const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
     let allBooksRes = {};
-    let { category_id, isNew, limit, currentPage } = req.query;
+    let { category_id, news, limit, currentPage } = req.query;
 
     let offset = limit * (currentPage - 1);
 
     let sql = `SELECT SQL_CALC_FOUND_ROWS *, 
                     (SELECT count(*) FROM likes WHERE books.id = liked_book_id) AS likes FROM books`;
     let values = [];
-    if (category_id && isNew) {
+    if (category_id && news) {
         sql += ' WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
         values = [category_id];
     } else if (category_id) {
         sql += ' WHERE category_id = ?';
         values = [category_id];
-    } else if (isNew) {
+    } else if (news) {
         sql += ' WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
     }
 
